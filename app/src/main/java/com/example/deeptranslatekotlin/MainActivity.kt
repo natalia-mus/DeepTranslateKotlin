aac2a2794ddd89293cae961fa5428a93ffa08150
natalia-mus <natalia.muskala1ab1@gmail.com>
2023-06-24 17:04:43 +0200

    Fragments

--- app/src/main/java/com/example/deeptranslatekotlin/MainActivity.kt
+++ app/src/main/java/com/example/deeptranslatekotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.deeptranslatekotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.main_bottom_navigation)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_translate_text -> changeCurrentFragment(TranslateTextFragment())
+                R.id.item_translate_file -> changeCurrentFragment(TranslateFileFragment())
+            }
+            true
+        }
+
+        changeCurrentFragment(TranslateTextFragment())
+
+    }
+
+    private fun changeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_fragment, fragment)
+            commit()
+        }
     }
 
 }
